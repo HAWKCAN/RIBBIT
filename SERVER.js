671bf01528f8f76b962b1e6e23d755eea3a2a261
HAWKCAN <farizrahman335577@gmail.com>
2025-04-12 13:18:52 +0700

    Update SERVER.js

--- SERVER.js
+++ SERVER.js
@@ -30,8 +30,10 @@ app.get('/' , (req,res) => {
 
 
 
-app.listen (3000 ,() => {
-    console.log('server udah nyala di http://localhost:3000');
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 });
 
