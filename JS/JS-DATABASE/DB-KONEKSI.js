2bb5a74ccb1e33379120ff6588daf6808ce0ba62
HAWKCAN <farizrahman335577@gmail.com>
2025-04-12 15:33:25 +0700

    update

--- JS/JS-DATABASE/DB-KONEKSI.js
+++ JS/JS-DATABASE/DB-KONEKSI.js
@@ -6,7 +6,8 @@ const db = mysql.createConnection({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'RIBBIT',
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
+  protocol: process.env.DB_PROTOCOL
 });
 
 db.connect(err => {
