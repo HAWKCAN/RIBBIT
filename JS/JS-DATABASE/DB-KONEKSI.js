38ac649e9caf8debd7fdaecf441b07dd6ecc607b
HAWKCAN <farizrahman335577@gmail.com>
2025-04-14 17:23:52 +0700

    ganti database

--- JS/JS-DATABASE/DB-KONEKSI.js
+++ JS/JS-DATABASE/DB-KONEKSI.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME || 'RIBBIT',
   port: process.env.DB_PORT || 3306,
   ssl: {
-    rejectUnauthorized: true // WAJIB true untuk Railway
+    rejectUnauthorized: false // WAJIB true untuk Railway
   }
 });
 
